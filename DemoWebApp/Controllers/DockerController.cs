546e69c3d609372f03f8e8af9fa3ddf5c7c34243
sarva <sarva@DESKTOP-NHQ1JG9>
2023-11-23 20:13:03 +0500

    Add project files.

---
+++ DemoWebApp/Controllers/DockerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DockerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Qaytar(string Name,int Age)
+        {
+            return Ok($"ism : {Name}  yosh: {Age}");
+        }
+    }
+}
